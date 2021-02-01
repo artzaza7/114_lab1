caa7144748459bcc9a3f1721c177db00bc2696ee
artzaza7 <75871892+artzaza7@users.noreply.github.com>
2021-02-01 18:53:25 +0700

    lab1_5 แก้ไข
    เปลี่ยนเงื่อนไขที่ยุ่งยากและมองโจทย์ให้ง่ายมากขึ้นครับ :D

--- lab1_5.c
+++ lab1_5.c
@@ -6,17 +6,13 @@ int main()
     int k[n];
     for(int i=0;i<n;i++)
         scanf("%d",&k[i]);
-    int check=k[0],position,a=0;
-    for(int i=0;i<n;i++)
-        if (check<k[i])
+    int check=k[0],position=0;
+    for(int i=1;i<n;i++)
+        if (k[i]>check)
             {
                 check=k[i];
-                position=i+1;
-                a++;
-
+                position=i;
             }
-        else
-            position=i-a-1;
-    printf("%d %d",position,check);
+    printf("%d %d",position+1,check);
     return 0;
 }
